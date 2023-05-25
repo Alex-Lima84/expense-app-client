c132c8a6dbfedf82027d15be7eda991e4332e7c5
Alexandre Luiz <alexandre.cerutti@live.com>
2023-05-25 15:10:15 -0300

    form implementation

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
 import { useCookies } from 'react-cookie'
 import './styles.scss'
 interface dataType {
@@ -22,43 +22,110 @@ const Modal = ({
         progress: editMode ? task.progress : 50,
         date: editMode ? task.date : new Date()
     })
+    const [expenseCategories, setExpenseCategories] = useState<any>('')
+    const [expenseCategoryName, setExpenseCategoryName] = useState<string>('')
+    const [expenseTypes, setExpenseTypes] = useState<any>('')
+    const [expenseTypeName, setExpenseTypeName] = useState<string>('')
 
-    const postData = async (e: any) => {
-        e.preventDefault()
+    const showExpenseCategory = async () => {
         try {
-            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/todos`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(data)
-            })
-            if (response.status === 200) {
-                console.log('new todo created')
-                setShowModal(false)
-                getData()
-            }
+
+            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/expense-categories`)
+            const data = await response.json()
+            setExpenseCategories(data)
+
         } catch (error) {
             console.error(error)
         }
     }
 
-    const editData = async (e: any) => {
-        e.preventDefault()
+    const getExpenseTypes = async (categoryId: string) => {
+
+        addCategoryName(categoryId)
 
         try {
-            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/todos/${task.id}`, {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(data)
-            })
-            if (response.status === 200) {
-                console.log('The todo was updated')
-                setShowModal(false)
-                getData()
-            }
+
+            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/expense-types/${categoryId}`)
+            const data = await response.json()
+            setExpenseTypes(data)
+
         } catch (error) {
             console.error(error)
         }
     }
+
+    const addCategoryName = (categoryId: string) => {
+        setExpenseCategoryName('')
+        setExpenseTypeName('')
+
+        switch (categoryId) {
+            case '581921fe-c8e9-4989-9a52-b6c5e5f368b4': {
+                setExpenseCategoryName('Habitação');
+                break;
+            }
+            case '18b03713-2dec-4ca2-9eea-7f5766289e4f': {
+                setExpenseCategoryName('Saúde');
+                break;
+            }
+            case '61a3ed30-224b-4945-9687-c22cb578e15f': {
+                setExpenseCategoryName('Transporte');
+                break;
+            }
+            case '4eb2deb6-7706-455f-a62f-b505b19a2fc3': {
+                setExpenseCategoryName('Automóvel');
+                break;
+            }
+            case 'd85451bd-b2d1-4ad2-bbde-50e62ce96218': {
+                setExpenseCategoryName('Despesas pessoais');
+                break;
+            }
+            case '50218ddf-dbdc-4483-a0ba-5c1f1f06c1ce': {
+                setExpenseCategoryName('Lazer');
+                break;
+            }
+            case '52f51e00-f5a3-4a83-b44a-e81bfd63f64d': {
+                setExpenseCategoryName('Educação');
+                break;
+            }
+        }
+    }
+
+    // const postData = async (e: any) => {
+    //     e.preventDefault()
+    //     try {
+    //         const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/todos`, {
+    //             method: 'POST',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify(data)
+    //         })
+    //         if (response.status === 200) {
+    //             console.log('new todo created')
+    //             setShowModal(false)
+    //             getData()
+    //         }
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+    // }
+
+    // const editData = async (e: any) => {
+    //     e.preventDefault()
+
+    //     try {
+    //         const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/todos/${task.id}`, {
+    //             method: 'PUT',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify(data)
+    //         })
+    //         if (response.status === 200) {
+    //             console.log('The todo was updated')
+    //             setShowModal(false)
+    //             getData()
+    //         }
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+    // }     
 
     const handleChange = (e: any) => {
 
@@ -68,6 +135,13 @@ const Modal = ({
             [name]: value
         }))
     }
+
+    useEffect(() => {
+        showExpenseCategory()
+    }, [])
+
+    console.log(expenseCategoryName)
+    console.log(expenseTypeName)
 
     return (
         <div className='overlay'>
@@ -85,22 +159,42 @@ const Modal = ({
                         value={data.title}
                         onChange={handleChange}
                     />
-                    <br />
-                    <label htmlFor='range'>Drag to select your current progress</label>
-                    <input
-                        required
-                        type='range'
-                        id='range'
-                        min='0'
-                        max='100'
-                        name='progress'
-                        value={data.progress}
-                        onChange={handleChange}
-                    />
+                    <div className='category-choice'>
+                        <label>Escolha a categoria da despesa</label>
+                        <select
+                            onChange={(e: any) => { getExpenseTypes(e.target.value) }}
+                        >
+                            <option value="">Selecione...</option>
+                            {expenseCategories ? expenseCategories.map((option: any) => (
+                                <option
+                                    key={option.id}
+                                    value={option.id}
+                                >
+                                    {option.expense_category}
+                                </option>
+                            )) : ''}
+                        </select >
+                    </div>
+                    <div className='type-choice'>
+                        <label>Escolha o tipo de despesa</label>
+                        <select
+                            onChange={(e: any) => setExpenseTypeName(e.target.value)}
+                        >
+                            <option value="">Selecione...</option>
+                            {expenseTypes ? expenseTypes.map((option: any) => (
+                                <option
+                                    key={option.id}
+                                    value={option.expense_type}
+                                >
+                                    {option.expense_type}
+                                </option>
+                            )) : ''}
+                        </select >
+                    </div>
                     <input
                         className={mode}
                         type='submit'
-                        onClick={editMode ? editData : postData}
+                    // onClick={editMode ? editData : postData}
                     />
                 </form>
             </div>
